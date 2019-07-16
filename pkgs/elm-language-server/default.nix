77296bd5090ee4db7680c44373a15022117d0c0c
Brian Hicks <brian@brianthicks.com>
2019-07-16 06:00:45 -0500

    put node-tree-sitter in the right place

--- pkgs/elm-language-server/default.nix
+++ pkgs/elm-language-server/default.nix
@@ -12,13 +12,13 @@ let
   node-versions-file = "${node-versions}/versions";
 
   node-tree-sitter-version = "v0.14.0";
-  node-tree-sitter = builtins.fetchurl "https://github.com/tree-sitter/node-tree-sitter/releases/download/${node-tree-sitter-version}/tree-sitter-${node-tree-sitter-version}-node-${builtins.readFile node-versions-file}.tar.gz";
+  node-tree-sitter-filename = "tree-sitter-${node-tree-sitter-version}-node-${builtins.readFile node-versions-file}.tar.gz";
+  node-tree-sitter = builtins.fetchurl "https://github.com/tree-sitter/node-tree-sitter/releases/download/${node-tree-sitter-version}/${node-tree-sitter-filename}";
 in
   nodePackages."@elm-tooling/elm-language-server-1.1.1".overrideAttrs
     (attrs: attrs // {
       name = "elm-language-server";
       preRebuild = ''
-        echo ${node-tree-sitter};
-        exit 1;
+        cp ${node-tree-sitter} node_modules/tree-sitter/${node-tree-sitter-filename}
       '';
     })
