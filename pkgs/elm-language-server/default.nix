a7345b1860cc7fda3b7f291b1e420c3d10fa3973
Brian Hicks <brian@brianthicks.com>
2019-07-16 06:22:00 -0500

    nicer comments

--- pkgs/elm-language-server/default.nix
+++ pkgs/elm-language-server/default.nix
@@ -10,10 +10,17 @@ let
   '';
   node-versions-file = "${node-versions}/versions";
 
+  ## get tarballs
+  # we need to get tarballs for runtime dependencies and put them in the right
+  # places so that the runtime does not try and download them itself. It won't
+  # have permissions at runtime, so that fails!
+
+  # node-tree-sitter tarball
   node-tree-sitter-version = "v0.14.0";
   node-tree-sitter-filename = "tree-sitter-${node-tree-sitter-version}-node-${builtins.readFile node-versions-file}.tar.gz";
   node-tree-sitter = builtins.fetchurl "https://github.com/tree-sitter/node-tree-sitter/releases/download/${node-tree-sitter-version}/${node-tree-sitter-filename}";
 
+  # tree-sitter-elm tarball
   tree-sitter-elm-version = "v2.4.1";
   tree-sitter-elm-filename = "tree-sitter-elm-${tree-sitter-elm-version}-node-${builtins.readFile node-versions-file}.tar.gz";
   tree-sitter-elm = builtins.fetchurl "https://github.com/Razzeee/tree-sitter-elm/releases/download/${tree-sitter-elm-version}/${tree-sitter-elm-filename}";
