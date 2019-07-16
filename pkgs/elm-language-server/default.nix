146668f02964aac89950190619d33c1c2587b8d0
Brian Hicks <brian@brianthicks.com>
2019-07-16 15:59:26 -0500

    unpack tarballs

--- pkgs/elm-language-server/default.nix
+++ pkgs/elm-language-server/default.nix
@@ -18,18 +18,18 @@ let
   # node-tree-sitter tarball
   node-tree-sitter-version = "v0.14.0";
   node-tree-sitter-filename = "tree-sitter-${node-tree-sitter-version}-node-${builtins.readFile node-versions-file}.tar.gz";
-  node-tree-sitter = builtins.fetchurl "https://github.com/tree-sitter/node-tree-sitter/releases/download/${node-tree-sitter-version}/${node-tree-sitter-filename}";
+  node-tree-sitter = builtins.fetchTarball "https://github.com/tree-sitter/node-tree-sitter/releases/download/${node-tree-sitter-version}/${node-tree-sitter-filename}";
 
   # tree-sitter-elm tarball
   tree-sitter-elm-version = "v2.4.1";
   tree-sitter-elm-filename = "tree-sitter-elm-${tree-sitter-elm-version}-node-${builtins.readFile node-versions-file}.tar.gz";
-  tree-sitter-elm = builtins.fetchurl "https://github.com/Razzeee/tree-sitter-elm/releases/download/${tree-sitter-elm-version}/${tree-sitter-elm-filename}";
+  tree-sitter-elm = builtins.fetchTarball "https://github.com/Razzeee/tree-sitter-elm/releases/download/${tree-sitter-elm-version}/${tree-sitter-elm-filename}";
 in
   nodePackages."@elm-tooling/elm-language-server-1.1.1".overrideAttrs
     (attrs: attrs // {
       name = "elm-language-server";
       preRebuild = ''
-        cp ${node-tree-sitter} ${node-tree-sitter-filename}
-        cp ${tree-sitter-elm} ${tree-sitter-elm-filename}
+        ln -s ${node-tree-sitter} node_modules/tree-sitter/${node-tree-sitter-filename}
+        ln -s ${tree-sitter-elm} node_modules/tree-sitter-elm/${tree-sitter-elm-filename}
       '';
     })
